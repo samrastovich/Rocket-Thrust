4ac92c8b1de46710c61dfa01ac939eca5e3d04eb
Sam Rastovich <sam@thinglogix.com>
2019-06-23 23:29:38 -0700

    Add new level and add death / next level controls`

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-
+    [SerializeField] float rcsThrust = 100f;
     Rigidbody rigidBody;
     AudioSource audioSource;
     bool engineIsRunning = false;
+
+    enum State
+    {
+        Alive, Dying, Transcending
+    }
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start() {
@@ -21,20 +28,63 @@ public class Rocket : MonoBehaviour
 
     void FixedUpdate()
     {
-        ProcessInput();
+        if (state == State.Alive)
+        {
+            Rotate();
+            Thrust();
+        }
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)
+        {
+            return;
+        }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Enemy":
+                state = State.Dying;
+                Invoke("LoadFirstScene", 1f);
+                break;
+            case "Friendly":
+                state = State.Alive;
+                break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f);
+                break;
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         }
-
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }
+    private void Thrust()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
@@ -43,6 +93,11 @@ public class Rocket : MonoBehaviour
                 audioSource.Play();
                 engineIsRunning = true;
             }
+        }
+        else
+        {
+            audioSource.Stop();
+            engineIsRunning = false;
         }
     }
 }
